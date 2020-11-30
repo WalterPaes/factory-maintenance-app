8dddd3b2d51e11633ff1d4cc7507bb65c1983383
Walter Junior <wpmj1277@gmail.com>
2020-11-29 23:30:55 -0300

    Improving Maintenance form

--- src/components/MaintenanceForm.jsx
+++ src/components/MaintenanceForm.jsx
@@ -3,16 +3,37 @@ import { Form, Button, ProgressBar, Alert } from "react-bootstrap";
 import PageLayout from "./layout/PageLayout";
 
 function MaintenanceForm() {
-    const [name, setName] = useState("");
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+    const [start, setStart] = useState("");
+    const [end, setEnd] = useState("");
+    const [description, setDescription] = useState("");
     const [btnSave] = useState("Salvar");
     const [btnCancel] = useState("Cancelar");
     
     return(
         <PageLayout pageTitle="Cadastro de Manutenção">
             <Form>
+                <Form.Group controlId="formStartDate">
+                    <Form.Control type="text" placeholder="Horário de Início" value={start} 
+                    onChange={(event) => {
+                        setStart(event.target.value)
+                    }}
+                    />
+                </Form.Group>
                 
+                <Form.Group controlId="formEndDate">
+                    <Form.Control type="text" placeholder="Horário do Fim" value={end} 
+                    onChange={(event) => {
+                        setEnd(event.target.value)
+                    }}/>
+                </Form.Group>
+
+                <Form.Group controlId="formDescription">
+                    <Form.Control as="textarea" rows={3}
+                    placeholder="Descrição" value={description}
+                    onChange={(event) => {
+                        setDescription(event.target.value)
+                    }}/>
+                </Form.Group>
 
                 <Button variant="primary" type="submit" size="sm" block>
                     { btnSave }
